7e8284938513a06163e9c58c07998258e7fde3a5
Lautaro Marguery <l.e.marguery@gmail.com>
2025-05-12 00:17:59 -0300

    Se corrigio la seleccion de idioma. Ahora si se muestran los iconos. Se trean las imagenes (.png) y se las importa como variable

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -4,7 +4,9 @@ import { IonFab, IonFabButton, IonIcon, IonFabList } from '@ionic/react';
 import { language } from 'ionicons/icons';
 import { useSettings, Language } from '../contexts/SettingsContext';
 import './LanguageSelector.css';
-
+import spainFlag from '../assets/flags/spain.png';
+import ukFlag from '../assets/flags/UK.png';
+import brazilFlag from '../assets/flags/brazil.png';
 const LanguageSelector: React.FC = () => {
   const { setLanguage } = useSettings();
 
@@ -19,13 +21,13 @@ const LanguageSelector: React.FC = () => {
       </IonFabButton>
       <IonFabList side="top">
         <IonFabButton onClick={() => handleLanguageChange('es')}>
-          <img src="/assets/flags/spain.png" alt="Español" className="flag-icon" />
+          <img src={spainFlag} alt="Español" className="flag-icon" />
         </IonFabButton>
         <IonFabButton onClick={() => handleLanguageChange('en')}>
-          <img src="/assets/flags/UK.png" alt="English" className="flag-icon" />
+          <img src={ukFlag} alt="English" className="flag-icon" />
         </IonFabButton>
         <IonFabButton onClick={() => handleLanguageChange('pt')}>
-          <img src="/assets/flags/brazil.png" alt="Português" className="flag-icon" />
+          <img src={brazilFlag} alt="Português" className="flag-icon" />
         </IonFabButton>
       </IonFabList>
     </IonFab>
